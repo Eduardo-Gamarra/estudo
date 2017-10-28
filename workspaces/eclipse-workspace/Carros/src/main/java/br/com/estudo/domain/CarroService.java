4de68b67257ebf3fbea67e982a04926c85e09b11
Beroy <gamarra.eduardo@gmail.com>
2017-10-28 15:00:48 -0200

    maven instalado

---
+++ workspaces/eclipse-workspace/Carros/src/main/java/br/com/estudo/domain/CarroService.java
@@ -0,0 +1,58 @@
+package br.com.estudo.domain;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarroService {
+	private CarroDAO db = new CarroDAO();
+	public List<Carro> getCarros(){
+		try {
+			List<Carro> carros = db.getCarros();
+			return carros;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return new ArrayList<Carro>();
+		}
+	}
+	public Carro getCarro(Long id) {
+		try {
+			return db.getCarroById(id);
+		}catch(SQLException e){
+			return null;
+		}
+	}
+	public boolean delete(Long id) {
+		try {
+			return db.delete(id);
+		}catch(SQLException e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean save(Carro carro) {
+		try {
+			db.save(carro);
+			return true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public List<Carro> findByName(String name){
+		try {
+			return db.findbyName(name);
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public List<Carro> findByTipo(String tipo){
+		try {
+			return db.findByTipo(tipo);
+		}catch(SQLException e) {
+			return null;
+		}
+	}
+}
